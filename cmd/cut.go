2b88402377fad1e464b78fb7bdabca990a52f376
Robert Ward <robert@rtward.com>
2020-08-11 16:53:46 -0500

    add the ability to select columns by name in the cut command

--- cmd/cut.go
+++ cmd/cut.go
@@ -39,12 +39,50 @@ func writeSelectedColumns(selectedColumns []int, csvWriter *csv.Writer, inputRec
 	}
 }
 
+func getSelectedColumns(cmd *cobra.Command) []int {
+	columnsByNum, err := cmd.Flags().GetIntSlice("columns")
+	if err != nil { log.Fatal().Err(err).Msg("error reading columns argument") }
+
+	if len(columnsByNum) > 0 {
+		log.Debug().Ints("selectedColumns", columnsByNum).Msg("got columns from columns arg")
+		return columnsByNum
+	}
+
+	columnsByName, err := cmd.Flags().GetStringSlice("column-names")
+	if err != nil { log.Fatal().Err(err).Msg("error reading columnn-names argument") }
+
+	if len(columnsByName) == 0 {
+		log.Debug().Msg("no columns arg provided, defaulting to all columns")
+		return nil
+	}
+
+	if !hasHeader { log.Fatal().Msg("cannot use column-names with CSV without a header") }
+
+	selectedColumns := make([]int, len(columnsByName))
+	for selectedColumnIdx := range columnsByName {
+		columnName := columnsByName[selectedColumnIdx]
+
+		found := false
+		for headerColumnIdx := range header {
+			headerColumn := header[headerColumnIdx]
+			if headerColumn == columnName {
+				selectedColumns = append(selectedColumns, headerColumnIdx)
+				found = true
+			}
+		}
+
+		if !found { log.Fatal().Str("column", columnName).Msg("unable to find column in the CSV header") }
+	}
+
+	log.Debug().Ints("selectedColumns", selectedColumns).Msg("got columns from column-names arg")
+	return selectedColumns
+}
+
 var cutCmd = &cobra.Command{
 	Use:   "cut",
 	Short: "Select one or more columns from a CSV file",
 	Run: func(cmd *cobra.Command, args []string) {
-		selectedColumns, err := cmd.Flags().GetIntSlice("columns")
-		if err != nil { log.Fatal().Err(err).Msg("error reading columns argument") }
+		selectedColumns := getSelectedColumns(cmd)
 
 		if hasHeader {
 			log.Debug().Strs("header", header).Msg("writing header row")
@@ -63,5 +101,6 @@ var cutCmd = &cobra.Command{
 
 func init() {
 	cutCmd.Flags().IntSliceP("columns", "c", nil, "a comma separated list of columns that should be extracted from the CSV")
+	cutCmd.Flags().StringSliceP("column-names", "n", nil, "a comma separated list of columns by name that should be extracted from the CSV")
 	rootCmd.AddCommand(cutCmd)
 }
